b3d292804f6e23147a814ba5992b3101eeed9524
mondev15 <evergiste.mutoni@gmail.com>
2021-05-10 16:59:08 +0200

    transclusion  des composants

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,10 +4,21 @@ import { IconsModule } from '../icons/icons.module';
 import { LoginModule } from '../login/login.module';
 import { TemplatesModule } from '../templates/templates.module';
 import { UiModule } from '../ui/ui.module';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent, NavComponent, FooterComponent],
   imports: [CommonModule],
-  exports: [UiModule, TemplatesModule, LoginModule, IconsModule],
+  exports: [
+    UiModule,
+    TemplatesModule,
+    LoginModule,
+    IconsModule,
+    HeaderComponent,
+    NavComponent,
+    FooterComponent,
+  ],
 })
 export class CoreModule {}
--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,15 +1,10 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { UiComponent } from './components/ui/ui.component';
 
-
-
 @NgModule({
-  declarations: [
-    UiComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [UiComponent],
+  imports: [CommonModule],
+  exports: [UiComponent],
 })
-export class UiModule { }
+export class UiModule {}
