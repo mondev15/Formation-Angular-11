91d1cd4506209c6785cf2321a009b3eb8937d880
mondev15 <evergiste.mutoni@gmail.com>
2021-05-11 16:51:57 +0200

    routerLink and routerLinkActive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,10 @@
 import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {
+  PreloadAllModules,
+  Router,
+  RouterModule,
+  Routes,
+} from '@angular/router';
 import { PageForgotComponent } from './login/pages/page-forgot/page-forgot.component';
 import { PageResetComponent } from './login/pages/page-reset/page-reset.component';
 import { PageSignInComponent } from './login/pages/page-sign-in/page-sign-in.component';
@@ -39,4 +44,10 @@ const routes: Routes = [
   ],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+  // accéder à la propriété public de confifg de la class Router
+  // pour afficher ma config dans la console
+  constructor(private router: Router) {
+    // console.log(this.router.config);
+  }
+}
